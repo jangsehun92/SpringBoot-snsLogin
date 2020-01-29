a9721e8b4592371380c5198bd3e0ca6496496d2e
장세훈 <jangsehun92@gmail.com>
2020-01-29 22:32:13 +0900

    add local project

---
+++ src/test/java/jsh/project/sns/DemoApplicationTests.java
@@ -0,0 +1,15 @@
+package jsh.project.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//window 환경에서 vscode java test는 잘 실행된다.
+		System.out.println("test중입니다.");
+	}
+
+}
