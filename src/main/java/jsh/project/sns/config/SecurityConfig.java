68fea00435ec91ebae3684a97be56fb6adaa8872
장세훈 <jangsehun92@gmail.com>
2020-02-05 23:22:53 +0900

    https 적용, springboot2.0 security5 에서 oauth2 로그인 공부중

--- src/main/java/jsh/project/sns/config/SecurityConfig.java
+++ src/main/java/jsh/project/sns/config/SecurityConfig.java
@@ -1,17 +1,14 @@
 package jsh.project.sns.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-//@EnableOAuth2Sso
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
 	/*
 	Spring 버젼이 2.x대로 넘어오면서 oauth 관련 설정들이 조금씩 바뀌어 전체적으로 다시 바꿔야할듯
 	*/
@@ -27,9 +24,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests()
 			.antMatchers("/","/login").permitAll()
 			.anyRequest().authenticated()
-                .and().logout().logoutSuccessUrl("/").permitAll()
-                .and().headers().frameOptions().sameOrigin()
-                .and().csrf().disable();
+				.and()
+			.logout().logoutSuccessUrl("/").permitAll()
+				.and()
+			.headers().frameOptions().sameOrigin()
+				.and()
+			.csrf().disable();
 	}
 
 }
--- src/main/java/jsh/project/sns/controller/LoginController.java
+++ src/main/java/jsh/project/sns/controller/LoginController.java
@@ -4,13 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
 @RestController
 public class LoginController{
 
-    // @GetMapping(value="/login")
-    // public String getMethodName() {
-    //     return "login";
-    // }
+    @GetMapping(value="/login")
+    public String getMethodName() {
+        return "home";
+    }
 
 }
