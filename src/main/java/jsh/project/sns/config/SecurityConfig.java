1d29d951e6f0c54c2e9880da0347344956416910
장세훈 <jangsehun92@gmail.com>
2020-02-06 23:35:02 +0900

    edit 예제에 맞춰 oauth2 버젼을 낮춤

--- src/main/java/jsh/project/sns/config/SecurityConfig.java
+++ src/main/java/jsh/project/sns/config/SecurityConfig.java
@@ -5,12 +5,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
-@Configuration
+import lombok.AllArgsConstructor;
+
+// @Configuration
+// @EnableWebSecurity
 @EnableWebSecurity
+@EnableOAuth2Client
+@AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	/*
 	Spring 버젼이 2.x대로 넘어오면서 oauth 관련 설정들이 조금씩 바뀌어 전체적으로 다시 바꿔야할듯
+	spring-security-oauth2 = 2.3.4.RELEASE
+	spring-security-oauth2-autoconfigure = 2.0.0.RELEASE 
+	버전으로 낮추니 특정 어노테이션 사용가능(일단 이렇게 예제 진행해야할듯하다)
 	*/
     @Override
 	public void configure(WebSecurity web) throws Exception {
@@ -30,6 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.headers().frameOptions().sameOrigin()
 				.and()
 			.csrf().disable();
+
 	}
 
 }
