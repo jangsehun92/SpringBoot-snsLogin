ee9b06680d65837072ca6f519aaf123d31b1f926
장세훈 <jangsehun92@gmail.com>
2020-02-07 10:45:11 +0900

    bean 추가

--- src/main/java/jsh/project/sns/config/SecurityConfig.java
+++ src/main/java/jsh/project/sns/config/SecurityConfig.java
@@ -1,11 +1,25 @@
 package jsh.project.sns.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.Filter;
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.web.filter.CompositeFilter;
 
 import lombok.AllArgsConstructor;
 
@@ -21,6 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	spring-security-oauth2-autoconfigure = 2.0.0.RELEASE 
 	버전으로 낮추니 특정 어노테이션 사용가능(일단 이렇게 예제 진행해야할듯하다)
 	*/
+
     @Override
 	public void configure(WebSecurity web) throws Exception {
 		//리소스 폴더 내에 css&lib 폴더를 두고 사용할 경우 추가해 주면 된다.
@@ -31,8 +46,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers("/","/login").permitAll()
-			.anyRequest().authenticated()
+			.antMatchers("/","/login").permitAll().anyRequest().authenticated()
 				.and()
 			.logout().logoutSuccessUrl("/").permitAll()
 				.and()
@@ -42,4 +56,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	}
 
+    @Bean
+    @ConfigurationProperties("google")
+    public ClientResources google() {
+        return new ClientResources();
+	}
+	
 }
